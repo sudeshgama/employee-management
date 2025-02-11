19968d3238b93388dedb76fd55f61aa56489fa9f
Sudesh Gama <sudeshgama@Mac.attlocal.net>
2025-02-10 22:00:43 -0600

    Aded Enum

--- src/app/tasks/store/reducer/task.reducer.ts
+++ src/app/tasks/store/reducer/task.reducer.ts
@@ -3,6 +3,12 @@ import { createFeature, createReducer, createSelector, on } from "@ngrx/store";
 import { SaveTasks, SaveTasksFailure, SaveTasksSuccess } from "../actions/task.actions";
 import { Task } from "../../models/task.model";
 
+export enum TaskStatus {
+  NEW = 'NEW',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED'
+}
+
 export const FeatureKey = 'task';
 
 export interface TaskState extends EntityState<Task> {
@@ -44,21 +50,21 @@ export const taskFeature = createFeature({
       selectTaskState,
       (state: TaskState) => {
         const allTasks = adapter.getSelectors().selectAll(state);
-        return allTasks.filter(task => task.status === 'in-progress')
+        return allTasks.filter(task => task.status === TaskStatus.IN_PROGRESS)
       }
     ),
     selectNewTasks: createSelector(
       selectTaskState,
       (state: TaskState) => {
         const allTasks = adapter.getSelectors().selectAll(state);
-        return allTasks.filter(task => task.status === 'in-progress')
+        return allTasks.filter(task => task.status === TaskStatus.NEW)
       }
     ),
     selectCompletedTasks: createSelector(
       selectTaskState,
       (state: TaskState) => {
         const allTasks = adapter.getSelectors().selectAll(state);
-        return allTasks.filter(task => task.status === 'in-progress')
+        return allTasks.filter(task => task.status === TaskStatus.COMPLETED)
       }
     )
   })
